c4a93fa31b998b20f1743e05dd217387e5c50e08
Christian Lozano <johndoe@example.com>
2022-09-29 11:36:20 -0500

    second commit

---
+++ React/Intro/first_react/my-first-project/src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    
+      <header className="App-header">
+        <div>
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do: </h2>
+        <p>
+          <ul>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+          </ul>
+        </p>
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
